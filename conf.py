29ef26b5c0b7f05932235c2017e475b13586731c
Rishiraj0100 <Rishiraj0100@users.noreply.github.com>
2022-01-29 05:27:34 +0000

    deploy: 05f3ad6d98260a0ae1e3f83b3c36998204bdc1b6

---
+++ conf.py
@@ -0,0 +1,16 @@
+project = "SBL.js"
+author = "Rishiraj0100"
+
+extensions = ["recommonmark", "sphinx_markdown_tables"]
+
+source_suffix = [".md", ".rst"]
+
+html_theme = "sphinx_rtd_theme"
+
+master_doc = "master"
+
+html_theme_options = {
+  "collapse_navigation": True,
+  "sticky_navigation": True,
+  "includehidden": True
+}
